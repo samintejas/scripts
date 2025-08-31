e3aa930cbdfb7f0b4f2efcd00328f6e79566547e
samintejas <samintejas@gmail.com>
2025-08-31 10:20:40 +0530

    latest stable

--- init.sh
+++ init.sh
@@ -1,42 +1,50 @@
 
-echo "installing packages .."
-sudo pacman -S git base-devel exa mesa vscode spotify-launcher stow helix intellij-idea-community-edition waybar hyprland hyprsunset rofi-wayland ghostty xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland imv nwg-look yazi starship fd zsh fzf swaync sddm bat swww grim slurp unzip uwsm docker docker-compose btop openssh cland cmake ninja wl-clipboard bluez bluez-utils
 
-echo "applying configs .. "
+echo "installing packages .."
+sudo pacman -S reflector git base-devel exa mesa vscode spotify-launcher stow intellij-idea-community-edition waybar hyprland hyprsunset rofi-wayland ghostty xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland imv nwg-look yazi starship fd zsh fzf swaync sddm bat swww grim slurp unzip uwsm docker docker-compose btop openssh wl-clipboard bluez bluez-utils
+
+echo "updating mirrors"
+reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+echo "resyncing repo"
+sudo pacman -Syy
+
+echo "pulling configs .. "
 
 cd $HOME
+mkdir -p $HOME/tmp
+
+cd tmp
 mkdir -p repo
 cd repo
 git clone https://github.com/samintejas/dots.git
 cd dots
-stow -t ~ git
+
+echo "configuring shell"
 stow -t ~ zsh
-stow -t ~/.config/ config
 
-echo "changing default shell to zsh .. "
-
-cd $HOME
-# root
 sudo chsh -s /bin/zsh
-# user
 sudo chsh -s /usr/bin/zsh $USER
 
+echo "creating config symlink"
+mkdir -p ~./.config
+stow -t ~/.config/ config
 
 echo "installing paru .. "
 
-cd $HOME
+cd $HOME/tmp/
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
+
 echo "installing additional packages from aur .. "
-cd $HOME
+cd $HOME/tmp/
 paru -S zen-browser
 paru -S ttf-jetbrains-mono-nerd
-paru -S mycli
 
-echo "next step 1 : download and install work git certificated"
-echo "next step 2 : add git credentials for personal and work"
-echo "next step 3 : setup systemd-boot to include windows if any"
+# echo "next step 1 : download and install work git certificated"
+# echo "next step 2 : add git credentials for personal and work"
+echo "next step 3 : setup grub"
 echo "next step 4 : setup duelboot optimizations if any"
 
